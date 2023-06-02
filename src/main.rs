55e5e698936a902e31ea72c1d13cb51c3981be8b
João Carvalho <joao.carvalho@tripleoak.pt>
2023-06-02 16:47:21 +0900

    base code with tui

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+
+mod screens;
+
+mod app;
+mod ui;
+
+mod crossterm;
+use crate::crossterm::run;
+
+use argh::FromArgs;
+use std::{error::Error, time::Duration};
+
+#[derive(Debug, FromArgs)]
+#[argh(description = "Client for ratatui")]
+struct Cli {
+    /// time in ms between two ticks.
+    #[argh(option, default = "250")]
+    tick_rate: u64,
+    /// whether unicode symbols are used to improve the overall look of the app
+    #[argh(option, default = "true")]
+    enhanced_graphics: bool,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let cli: Cli = argh::from_env();
+    let tick_rate = Duration::from_millis(cli.tick_rate);
+    run(tick_rate, cli.enhanced_graphics)?;
+    Ok(())
+}
